df36f6db30621805ebea01cf05427281eb824602
heart <7362469@qq.com>
2023-08-23 21:28:50 +0800

    perf: optimize keydown `enter` to login

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -7,6 +7,8 @@ import { useRouter } from 'vue-router'
 import { findSecurity } from '@/model/security'
 import { useProfile } from '@/storage/user'
 import { isNull } from '@cc-heart/utils'
+import { onMounted } from 'vue'
+import { onUnmounted } from 'vue'
 const router = useRouter()
 const userInfo = reactive({
   username: '',
@@ -32,24 +34,29 @@ const handleSubmit = async () => {
     }
   }
 }
+
+function listenerEnterToLogin(e: KeyboardEvent) {
+  if (e.key === 'Enter') {
+    handleSubmit()
+  }
+}
+onMounted(() => {
+  window.addEventListener('keypress', listenerEnterToLogin)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('keypress', listenerEnterToLogin)
+})
+
 </script>
 <template>
   <div class="h-full w-full flex justify-center items-center">
     <div class="w-80 text-center">
-      <Input
-        class="m-b-6"
-        v-model:value="userInfo.username"
-        :placeholder="$t('pages-index.usernamePlaceholder')"
-      />
-      <InputPassword
-        v-model:value="userInfo.password"
-        :placeholder="$t('pages-index.passwordPlaceholder')"
-      />
+      <Input class="m-b-6" v-model:value="userInfo.username" :placeholder="$t('pages-index.usernamePlaceholder')" />
+      <InputPassword v-model:value="userInfo.password" :placeholder="$t('pages-index.passwordPlaceholder')" />
       <Button class="m-t-6" @click="handleSubmit">{{
         $t('pages-index.submitButton')
       }}</Button>
     </div>
   </div>
 </template>
-<style lang="scss"></style>
-@/features/user/api

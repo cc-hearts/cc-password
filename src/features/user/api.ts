a23930232fb96c13401acd3a9dd6cddcefb92596
heart <7362469@qq.com>
2023-09-04 22:53:56 +0800

    perf: remove unless import

--- src/features/user/api.ts
+++ src/features/user/api.ts
@@ -1,6 +1,5 @@
 import { Get, Post } from '@/utils/request.ts'
 import type { ILoginApi, ILoginParams, Profile } from './types'
-import { getRefreshToken } from '@/storage'
 
 export async function loginApi<T extends ILoginParams>(data: T) {
   const resultApi = Post<ILoginApi>(`user/login`, data)
